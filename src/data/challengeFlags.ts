9821a8eb04bad3cd80b5b80399272a67946818fa
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-01-31 11:12:09 +0530

    made changes to the format

--- src/data/challengeFlags.ts
+++ src/data/challengeFlags.ts
@@ -8,5 +8,5 @@ export const challengeFlags = {
   audio : "CSCTF{its_the_king_of_jack}",
   forensics7 : "CSCTF{Backup_Datacenter_Destroyed}",
   crypto : "CSCTF{P0larB33r}",
-  Web : "CSCTF{w3lcom3_t0_th3_j0urn3y}"
+  Web : "CSCTF{{3wlcom3_t0_th3_j0urn3y}}"
 }; 
